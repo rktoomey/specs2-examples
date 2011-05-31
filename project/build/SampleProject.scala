9b108341c97e3a786f49889e659acfd65d47c3fe
rktoomey <rktoomey@gmail.com>
2011-05-31 07:11:57 -0400

    Updated specs2 sample deps to Scala 2.9.0-1 and specs2 1.4-SNAPSHOT

--- project/build/SampleProject.scala
+++ project/build/SampleProject.scala
@@ -4,7 +4,7 @@ class SampleProject(info: ProjectInfo) extends DefaultProject(info) {
     
     override def compileOptions = super.compileOptions ++ Seq(Unchecked, Deprecation)
     
-     val specs2 = "org.specs2" %% "specs2" % "1.3"  
+     val specs2 = "org.specs2" %% "specs2" % "1.4-SNAPSHOT"  
      val scalaz = "org.specs2" %% "scalaz-core" % "6.0.RC2"  
      
      def specs2Framework = new TestFramework("org.specs2.runner.SpecsFramework")
